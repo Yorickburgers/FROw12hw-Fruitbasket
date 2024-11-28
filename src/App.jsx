4230157aa1c7ba8d5fd61a0fb3383c23282a847b
yorickburgers <yorickburgers@live.nl>
2024-11-28 23:04:03 +0100

    Opdracht 2 done (?)

--- src/App.jsx
+++ src/App.jsx
@@ -27,20 +27,29 @@ function App() {
     };
 
     const [formState, setFormState] = useState({
-        firstname: '',
-        lastname: '',
+        firstName: '',
+        lastName: '',
         age: '0',
         zipCode: '',
-
+        frequency: 'iedere week',
+        time: 'night',
+        comments: '',
+        termsConditions: false
     });
 
     function handleChange(e) {
         const changedFieldName = e.target.name;
+        const newValue = e.target.type === "checkbox" ? e.target.checked : e.target.value;
 
         setFormState({
             ...formState,
-            [changedFieldName]: e.target.value,
+            [changedFieldName]: newValue,
         });
+    }
+
+    function logResults() {
+        console.log(counters);
+        console.log(formState);
     }
 
     return (
@@ -76,31 +85,31 @@ function App() {
             </section>
             <form>
                 <label htmlFor="firstName">Voornaam
-                    <input type="text" id="firstName" name="firstName"/></label>
+                    <input type="text" id="firstName" name="firstName" value={formState.firstName} onChange={handleChange}/></label>
                 <label htmlFor="lastName">Achternaam
-                    <input type="text" id="lastName" name="lastName"/></label>
+                    <input type="text" id="lastName" name="lastName" value={formState.lastName} onChange={handleChange}/></label>
                 <label htmlFor="age">Leeftijd
-                    <input type="number" id="age" name="age"/></label>
+                    <input type="number" id="age" name="age" value={formState.age} onChange={handleChange}/></label>
                 <label htmlFor="zipCode">Postcode
-                    <input type="text" id="zipCode" name="zipCode"/></label>
+                    <input type="text" id="zipCode" name="zipCode" value={formState.zipCode} onChange={handleChange}/></label>
                 <label>Bezorgfrequentie
-                    <select name="frequency">
+                    <select name="frequency" value={formState.frequency} onChange={handleChange}>
                         <option value="iedere week">Iedere week</option>
                         <option value="om de week">Om de week</option>
                         <option value="iedere maand">Iedere maand</option>
                     </select></label>
-                <label>
-                    <input type="radio" name="time"/>Overdag</label>
-                <label>
-                    <input type="radio" name="time"/>'s avonds</label>
-                <label htmlFor="comments">Opmerking
-                    <input type="field" id="comments" name="lastName"/></label>
-                <label>
-                    <input type="checkbox" name="lastName"/>Ik ga akkoord met de voorwaarden</label>
-                <button type="submit">Verzend</button>
+                <label htmlFor="time-day">
+                    <input type="radio" id="time-day" value="day" name="time" checked={formState.time === "day"} onChange={handleChange}/>Overdag</label>
+                <label htmlFor="time-night">
+                    <input type="radio" id="time-night" value="night" name="time" checked={formState.time === "night"} onChange={handleChange}/>'s avonds</label>
+                <label htmlFor="comments">Opmerkingen
+                    <input type="textarea" id="comments" name="comments" value={formState.comments} onChange={handleChange}/></label>
+                <label htmlFor="termsConditions">
+                    <input type="checkbox" id="termsConditions" name="termsConditions" value={formState.termsConditions} onChange={handleChange}/>Ik ga akkoord met de voorwaarden</label>
+                <button type="button" onClick={logResults}>Verzend</button>
             </form>
         </>
-    )
+    );
 }
 
 export default App
