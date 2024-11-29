b80ccccadcd34aad615eeaf40cdf686dee6ef31a
yorickburgers <yorickburgers@live.nl>
2024-11-29 18:12:08 +0100

    Opdracht 3 done

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,23 @@
+import './Input.css';
+import React from "react";
+
+function Input({inputID, inputLabel, inputType, value, updateCounter}) {
+    return (
+        <>
+            <label htmlFor={inputID}>{inputLabel}
+                <input
+                    type={inputType}
+                    id={inputID}
+                    name={inputID}
+                    value={value}
+                    onChange={updateCounter}/>
+            </label>
+        </>
+    );
+}
+
+export default Input;
+/*
+
+<label htmlFor="firstName">Voornaam
+    <input type="text" id="firstName" name="firstName" value={formState.firstName} onChange={handleChange}/></label>*/
